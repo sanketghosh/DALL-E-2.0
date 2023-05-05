d6174a3cf590206da19188e2d6a86ac3f57d95c7
Sanket Ghosh <code.sanket13@gmail.com>
2023-05-06 02:02:58 +0530

    adding readme and adding tailwind configs and fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="font-playfair">DALL-E CLONE</div>;
+  return <div className="font-playfair">DALL.E-2.0: A DALL.E CLONE</div>;
 };
 
 export default App;
